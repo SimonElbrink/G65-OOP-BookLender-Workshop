9dfedc0c587581203985e62fb718131807944a87
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-07-03 12:51:51 +0300

    Class Book: created method to check for empty fields firstName and lastName

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -6,11 +6,18 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    public Person (String firstName, String lastName){
+    public Person(String firstName, String lastName) {
+        if(firstName == null || firstName.trim().isEmpty()){
+            throw new IllegalArgumentException("First name cannot be null or empty");
+        }
+        if(lastName == null || lastName.trim().isEmpty()){
+            throw new IllegalArgumentException("Last name cannot be null or empty");
+        }
         this.id = getNextId();
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
     private static int getNextId() {
         return sequencer++;
     }
