14b45a0621cac9215b88348e25031b980dcbd6b8
Mykyta Shadrin <nikitashadriiiin@gmail.com>
2025-07-04 00:30:05 +0200

    Constructors and getNextId

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -7,4 +7,37 @@ package se.lexicon.model;
  */
 public class Book {
     // todo: needs completion
+    private String id;
+    private String title;
+    private String author;
+    private String available;
+    private Person borrower;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+
+    public Book(String title, String author, Person borrower){
+        this.title = title;
+        this.author = author;
+        this.borrower = borrower;
+    }
+
+    /**
+     * geters
+     * seters
+     * */
+
+
+    /**
+     * other methods
+     * */
+
+    public String getBookInformation(){
+        return null;
+    }
+
+
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -5,4 +5,46 @@ package se.lexicon.model;
  */
 public class Person {
     // todo: needs completion
+    private int sequencer = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName){
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("First name can't be empty or null");
+        }
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Lastname can't be empty or null");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = getNextId();
+    }
+
+    /**
+     * geters
+     * seters
+     * */
+
+
+    /**
+     * other methods
+     * */
+
+    private int getNextId(){
+        return ++sequencer;
+    }
+
+    public void loanBook(String book){
+
+    }
+
+    public void returnBook(String book){
+
+    }
+
+    public String getPersonInformation(){
+        return null;
+    }
 }
