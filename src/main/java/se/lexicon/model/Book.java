6c10d8549c831c8dcd1c83d282ff4019fc7917e5
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2025-07-03 12:21:01 +0200

    added some method to person and book

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,10 +1,78 @@
 package se.lexicon.model;
 
 
+import java.util.UUID;
+
 /**
  * This class represents a Book model with properties and methods
  * to manage book-related information and operations.
  */
 public class Book {
-    // todo: needs completion
+    private final String id = UUID.randomUUID().toString();
+    private String title;
+    private String author;
+    private boolean available = true;
+    private Person borrower = null;
+
+    public Book(String title, String author) {
+
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title, String author, Person borrower) {
+        this(title, author);
+        borrow(borrower);
+
+    }
+
+    public boolean borrow(Person p) {
+        if (!available || p == null) return false;
+        available = false;
+        borrower = p;
+        p.addBorrowedBook(this);
+        return true;
+    }
+
+    public boolean returnBook() {
+        if (available) return false;
+        borrower.removeBorrowedBook(this);
+        borrower = null;
+        available = true;
+        return true;
+    }
+    /* Getters */
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public Person getBorrower() {
+        return borrower;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                ", borrower=" + (borrower != null ? borrower.getPersonInformation() : "none") +
+                '}';
+    }
+
+
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,8 +1,57 @@
 package se.lexicon.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class represents a Person model with properties and methods
  * to manage personal details and interactions with the library system.
  */
 public class Person {
     // todo: needs completion
+    public static int sequencer = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private final List<Book> borrowedBooks = new ArrayList<>();
+
+    public Person(String firstName, String lastName) {
+        this.id = ++sequencer;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+
+
+    }
+
+
+    //
+    public int getNext() {
+        return ++ sequencer;
+    }
+    public boolean loanBook(Book book)   { return book != null && book.borrow(this); }
+    public boolean returnBook(Book book) { return book != null && book.returnBook(); }
+
+
+    void addBorrowedBook(Book b)    { borrowedBooks.add(b); }
+    void removeBorrowedBook(Book b) { borrowedBooks.remove(b); }
+
+
+
+    public String getPersonInformation() {
+        StringBuilder booksInfo = new StringBuilder();
+        for (Book book : borrowedBooks) {
+            booksInfo.append("[").append(book.getTitle()).append(" by ").append(book.getAuthor()).append("], ");
+        }
+
+        if (booksInfo.length() > 0) booksInfo.setLength(booksInfo.length() - 2);
+
+        return "Person{" +
+                "id: " + id +
+                ", firstName: '" + firstName + '\'' +
+                ", lastName: '" + lastName + '\'' +
+                ", loaned books: " + booksInfo +
+                '}';
+    }
+
 }
