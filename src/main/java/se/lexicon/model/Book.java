14d26c5e6d1558cb2f5c93291206ad70936c119a
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-07-03 12:09:12 +0300

    Class Book: created fields - id, title, author, boolean available, Person borrower; created constructor with args title, author and overloading constructor with title, author, Person borrower. Used setter for Available and getter for all fields.

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,10 +1,39 @@
 package se.lexicon.model;
 
 
-/**
- * This class represents a Book model with properties and methods
- * to manage book-related information and operations.
- */
+
 public class Book {
-    // todo: needs completion
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    public Book(String title, String author) {
+        this.title=title;
+        this.author=author;
+    }
+    public Book (String title, String author, Person borrower) {
+        this.title = title;
+        this.author = author;
+        this.borrower = borrower;
+    }
+    public void setAvailable(boolean available) {
+        this.available=available;
+        }
+    public String getId(){
+        return id;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Person getBorrower() {
+        return borrower;
+    }
 }
