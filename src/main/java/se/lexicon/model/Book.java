651e8f95afab8ae709f03540ae1903671ce022ec
sebbe fyr <sebbe.fyhr@hotmail.com>
2025-07-03 10:12:19 +0200

    Book and Person class implemented according to UML chart

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -6,5 +6,72 @@ package se.lexicon.model;
  * to manage book-related information and operations.
  */
 public class Book {
-    // todo: needs completion
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    public Book(String title, String author){
+       this.title = title;
+       this.author = author;
+    }
+    public Book(String title, String author, Person borrower){
+        this.title = title;
+        this.author = author;
+        this.borrower = borrower;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Person getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Person borrower) {
+        this.borrower = borrower;
+    }
+
+    public String getBookInfo(){
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                ", borrower=" + borrower +
+                '}';
+    }
+
+
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,8 +1,96 @@
 package se.lexicon.model;
+
+import java.util.Arrays;
+
 /**
  * This class represents a Person model with properties and methods
  * to manage personal details and interactions with the library system.
  */
 public class Person {
-    // todo: needs completion
+    private static int sequencer = 0;
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    private Book loanedBook;
+
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public void loanBook(Book book){
+        if(book.isAvailable()){
+            book.setAvailable(false);
+            book.setBorrower(this);
+            loanedBook = book;
+        }
+        else {
+            System.out.println("Book is not available");
+
+        }
+    }
+
+    public void returnBook(Book book){
+        book.setAvailable(true);
+        loanedBook = null;
+        book.setBorrower(null);
+    }
+
+    private int getNextId(){
+        return ++sequencer;
+    }
+
+    public static int getSequencer() {
+        return sequencer;
+    }
+
+    public static void setSequencer(int sequencer) {
+        Person.sequencer = sequencer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Book getLoanedBook() {
+        return loanedBook;
+    }
+
+    public void setLoanedBook(Book loanedBook) {
+        this.loanedBook = loanedBook;
+    }
+
+
+    public String getPersonInfo() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + loanedBook +
+                '}';
+    }
 }
